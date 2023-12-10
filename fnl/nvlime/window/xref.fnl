41d57bb5d68297b9e333551400d48b3041f58211
monkoose <pythonproof@gmail.com>
2023-12-10 03:10:21 +0200

    Refactor xref window

--- fnl/nvlime/window/xref.fnl
+++ fnl/nvlime/window/xref.fnl
@@ -2,17 +2,26 @@
 (local buffer (require "nvlime.buffer"))
 (local psl (require "parsley"))
 
+(local {: nvim_create_namespace
+        : nvim_create_autocmd
+        : nvim_buf_clear_namespace
+        : nvim_buf_line_count
+        : nvim_buf_set_extmark
+        : nvim_win_get_cursor
+        : nvim_win_set_cursor}
+       vim.api)
+
 (local xref {})
 
 (local +filetype+ (buffer.gen-filetype buffer.names.xref))
-(local +namespace+ (vim.api.nvim_create_namespace +filetype+))
+(local +namespace+ (nvim_create_namespace +filetype+))
 
 (var *last-line* 1)
 (var *prev-line* 0)
 
 ;;; [any] -> [string]
 (fn content->lines [content]
-  (var lines [])
+  (local lines [])
   (each [_ xref (ipairs content)]
     (let [filename (?. xref 2 2 2)
           sym (string.gsub (. xref 1) "\n%s*" " ")]
@@ -22,9 +31,9 @@
 
 ;;; LineNr -> integer
 (fn double-cursorline [linenr]
-  (vim.api.nvim_buf_clear_namespace
+  (nvim_buf_clear_namespace
     0 +namespace+ 0 -1)
-  (vim.api.nvim_buf_set_extmark
+  (nvim_buf_set_extmark
     0 +namespace+ (- linenr 1) 0
     {:end_row (+ linenr 1)
      :end_col 0
@@ -33,17 +42,14 @@
 
 ;;; WinID ->
 (fn move-odds-only [winid]
-  (let [[cur-line cur-col] (vim.api.nvim_win_get_cursor 0)]
+  (let [[cur-line cur-col] (nvim_win_get_cursor 0)]
     (when (psl.even? cur-line)
       (if (= cur-line *last-line*)
-          (vim.api.nvim_win_set_cursor
-            winid [(- cur-line 1) cur-col])
+          (nvim_win_set_cursor winid [(- cur-line 1) cur-col])
           (< *prev-line* cur-line)  ; elseif
-          (vim.api.nvim_win_set_cursor
-            winid [(+ cur-line 1) cur-col])
+          (nvim_win_set_cursor winid [(+ cur-line 1) cur-col])
           (> *prev-line* cur-line)  ; elseif
-          (vim.api.nvim_win_set_cursor
-            winid [(- cur-line 1) cur-col])))
+          (nvim_win_set_cursor winid [(- cur-line 1) cur-col])))
     (let [cur-line (vim.fn.line ".")]
       (set *prev-line* cur-line)
       (double-cursorline cur-line))))
@@ -51,8 +57,8 @@
 ;;; WinID BufNr ->
 (fn win-callback [winid bufnr]
   (set *prev-line* 0)
-  (set *last-line* (vim.api.nvim_buf_line_count bufnr))
-  (vim.api.nvim_create_autocmd
+  (set *last-line* (nvim_buf_line_count bufnr))
+  (nvim_create_autocmd
     "CursorMoved" {:buffer bufnr
                    :callback #(move-odds-only winid)}))
 
--- lua/nvlime/window/xref.lua
+++ lua/nvlime/window/xref.lua
@@ -1,9 +1,17 @@
 local window = require("nvlime.window")
 local buffer = require("nvlime.buffer")
 local psl = require("parsley")
+local _local_1_ = vim.api
+local nvim_create_namespace = _local_1_["nvim_create_namespace"]
+local nvim_create_autocmd = _local_1_["nvim_create_autocmd"]
+local nvim_buf_clear_namespace = _local_1_["nvim_buf_clear_namespace"]
+local nvim_buf_line_count = _local_1_["nvim_buf_line_count"]
+local nvim_buf_set_extmark = _local_1_["nvim_buf_set_extmark"]
+local nvim_win_get_cursor = _local_1_["nvim_win_get_cursor"]
+local nvim_win_set_cursor = _local_1_["nvim_win_set_cursor"]
 local xref = {}
 local _2bfiletype_2b = buffer["gen-filetype"](buffer.names.xref)
-local _2bnamespace_2b = vim.api.nvim_create_namespace(_2bfiletype_2b)
+local _2bnamespace_2b = nvim_create_namespace(_2bfiletype_2b)
 local _2alast_line_2a = 1
 local _2aprev_line_2a = 0
 local function content__3elines(content)
@@ -11,58 +19,58 @@ local function content__3elines(content)
   for _, xref0 in ipairs(content) do
     local filename
     do
-      local t_1_ = xref0
-      if (nil ~= t_1_) then
-        t_1_ = (t_1_)[2]
+      local t_2_ = xref0
+      if (nil ~= t_2_) then
+        t_2_ = t_2_[2]
       else
       end
-      if (nil ~= t_1_) then
-        t_1_ = (t_1_)[2]
+      if (nil ~= t_2_) then
+        t_2_ = t_2_[2]
       else
       end
-      if (nil ~= t_1_) then
-        t_1_ = (t_1_)[2]
+      if (nil ~= t_2_) then
+        t_2_ = t_2_[2]
       else
       end
-      filename = t_1_
+      filename = t_2_
     end
-    local sym = string.gsub((xref0)[1], "\n%s*", " ")
+    local sym = string.gsub(xref0[1], "\n%s*", " ")
     table.insert(lines, sym)
-    local function _5_()
-      local t_6_ = xref0
-      if (nil ~= t_6_) then
-        t_6_ = (t_6_)[2]
+    local function _6_(...)
+      local t_7_ = xref0
+      if (nil ~= t_7_) then
+        t_7_ = t_7_[2]
       else
       end
-      if (nil ~= t_6_) then
-        t_6_ = (t_6_)[1]
+      if (nil ~= t_7_) then
+        t_7_ = t_7_[1]
       else
       end
-      if (nil ~= t_6_) then
-        t_6_ = (t_6_).name
+      if (nil ~= t_7_) then
+        t_7_ = t_7_.name
       else
       end
-      return t_6_
+      return t_7_
     end
-    table.insert(lines, ("  ;; " .. (filename or _5_())))
+    table.insert(lines, ("  ;; " .. (filename or _6_())))
   end
   return lines
 end
 local function double_cursorline(linenr)
-  vim.api.nvim_buf_clear_namespace(0, _2bnamespace_2b, 0, -1)
-  return vim.api.nvim_buf_set_extmark(0, _2bnamespace_2b, (linenr - 1), 0, {end_row = (linenr + 1), end_col = 0, hl_eol = true, hl_group = "CursorLine"})
+  nvim_buf_clear_namespace(0, _2bnamespace_2b, 0, -1)
+  return nvim_buf_set_extmark(0, _2bnamespace_2b, (linenr - 1), 0, {end_row = (linenr + 1), end_col = 0, hl_eol = true, hl_group = "CursorLine"})
 end
 local function move_odds_only(winid)
-  local _let_10_ = vim.api.nvim_win_get_cursor(0)
-  local cur_line = _let_10_[1]
-  local cur_col = _let_10_[2]
+  local _let_11_ = nvim_win_get_cursor(0)
+  local cur_line = _let_11_[1]
+  local cur_col = _let_11_[2]
   if psl["even?"](cur_line) then
     if (cur_line == _2alast_line_2a) then
-      vim.api.nvim_win_set_cursor(winid, {(cur_line - 1), cur_col})
+      nvim_win_set_cursor(winid, {(cur_line - 1), cur_col})
     elseif (_2aprev_line_2a < cur_line) then
-      vim.api.nvim_win_set_cursor(winid, {(cur_line + 1), cur_col})
+      nvim_win_set_cursor(winid, {(cur_line + 1), cur_col})
     elseif (_2aprev_line_2a > cur_line) then
-      vim.api.nvim_win_set_cursor(winid, {(cur_line - 1), cur_col})
+      nvim_win_set_cursor(winid, {(cur_line - 1), cur_col})
     else
     end
   else
@@ -73,18 +81,18 @@ local function move_odds_only(winid)
 end
 local function win_callback(winid, bufnr)
   _2aprev_line_2a = 0
-  _2alast_line_2a = vim.api.nvim_buf_line_count(bufnr)
-  local function _13_()
+  _2alast_line_2a = nvim_buf_line_count(bufnr)
+  local function _14_()
     return move_odds_only(winid)
   end
-  return vim.api.nvim_create_autocmd("CursorMoved", {buffer = bufnr, callback = _13_})
+  return nvim_create_autocmd("CursorMoved", {buffer = bufnr, callback = _14_})
 end
 xref.open = function(content, config)
   local lines = content__3elines(content)
   local bufnr = buffer["create-scratch-with-conn-var!"](buffer["gen-name"](config["conn-name"], buffer.names.xref), _2bfiletype_2b)
-  local function _14_(_241, _242)
+  local function _15_(_241, _242)
     return win_callback(_241, _242)
   end
-  return {window.center.open(bufnr, lines, {width = 80, height = 10, title = buffer.names.xref}, _14_), bufnr}
+  return {window.center.open(bufnr, lines, {width = 80, height = 10, title = buffer.names.xref}, _15_), bufnr}
 end
 return xref
